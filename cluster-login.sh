9d444ec268da58b98ee93e3de86d0731d39dd688
Jermila Dhas <dhas.jermila@gmail.com>
2018-09-05 16:47:51 +0100

    Display url changes

--- cluster-login.sh
+++ cluster-login.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-output=$(kubectl-login $1)
-if [ $? -eq 0  ]
+output=$(./kubectl-login $1 | tee /dev/stderr)
+
+kubectlLoginOutput=($output)
+
+if [ ${#kubectlLoginOutput[@]} -eq 2  ]
+then
+    export KUBECONFIG=${output} | awk '{print $2; }'
+    echo "Logged in to $1. Using KUBECONFIG=$KUBECONFIG"
+elif [ ${#kubectlLoginOutput[@]} -eq 1 ]
 then
     export KUBECONFIG=${output}
     echo "Logged in to $1. Using KUBECONFIG=$KUBECONFIG"
--- main.go
+++ main.go
@@ -73,8 +73,13 @@ func main() {
 		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "groups", "offline_access"}, // "openid" is a required scope for OpenID Connect flows.
 	}
 
-	if err = openBrowser(oauth2Config.AuthCodeURL(state)); err != nil {
-		logger.Fatalf("error: cannot open browser: %v", err)
+	redirectUrl := oauth2Config.AuthCodeURL(state)
+	logger.Println(redirectUrl)
+	err = openBrowser(redirectUrl)
+ 	if err != nil {
+		if err != exec.ErrNotFound {
+			logger.Fatalf("error: cannot open browser: %v", err)
+		}
 	}
 
 	idTokenVerifier := provider.Verifier(&oidc.Config{ClientID: clientID})
