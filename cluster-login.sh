02f7da421c4b4db6c86319b1e9ef01989ecdb3a2
Jermila Dhas <dhas.jermila@gmail.com>
2018-09-06 13:26:37 +0100

    update comments

--- cluster-login.sh
+++ cluster-login.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-output=$(./kubectl-login $1 | tee /dev/stderr)
+output=$(kubectl-login $1 | tee /dev/stderr)
 
 kubectlLoginOutput=($output)
 
+# Output returns the redirect url (for Ops) and the kubeconfig
+# Get the kubeconfig path and set the KUBECONFIG environment varible
 if [ ${#kubectlLoginOutput[@]} -eq 2  ]
 then
     export KUBECONFIG=${output} | awk '{print $2; }'
     echo "Logged in to $1. Using KUBECONFIG=$KUBECONFIG"
+# Output returns the kubeconfig path which is set an the KUBECONFIG environment varible
 elif [ ${#kubectlLoginOutput[@]} -eq 1 ]
 then
     export KUBECONFIG=${output}
