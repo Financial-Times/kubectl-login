1078ecb937bb660d2d57fccc200ca5e35242a416
Jermila Dhas <dhas.jermila@gmail.com>
2018-09-18 13:26:48 +0100

    Get the kubeconfig path

--- cluster-login.sh
+++ cluster-login.sh
@@ -7,13 +7,12 @@ kubectlLoginOutput=($output)
 # Get the kubeconfig path and set the KUBECONFIG environment varible
 if [ ${#kubectlLoginOutput[@]} -eq 2  ]
 then
-    export KUBECONFIG=${kubectlLoginOutput[1]} | awk '{print $2; }'
+    kubeconfigPath=$(echo ${output} | awk '{print $2; }')
+    export KUBECONFIG=${kubeconfigPath}
     echo "Logged in to $1. Using KUBECONFIG=$KUBECONFIG"
 # Output returns the kubeconfig path which is set an the KUBECONFIG environment varible
 elif [ ${#kubectlLoginOutput[@]} -eq 1 ]
 then
     export KUBECONFIG=${output}
     echo "Logged in to $1. Using KUBECONFIG=$KUBECONFIG"
-else
-    echo "$output"
 fi
